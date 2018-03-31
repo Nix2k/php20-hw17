c090f1defa57626ce849c379a1bebc40c81c6800
Nix2k <mihail.bokov@gmail.com>
2018-04-01 01:06:01 +0300

    v2.1

--- templates/main.php
+++ templates/main.php
@@ -10,7 +10,7 @@
 	<form action="index.php" method="GET">
 		<input type="hidden" name="contr" value="task">
 		<input type="hidden" name="act" value="add">
-		<input type="text" name="desc" placeholder="Описание">
+		<input type="text" name="desc" placeholder="Описание" style="display: inline-block; min-width: 360px;">
 		<input type="submit" name="submit" value="Добавить задачу">
 	</form>
 	<h2>Созданные мной задачи</h2>
